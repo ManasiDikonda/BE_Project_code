b2b4429821128218828e374ef2159b24d254d6ae
Admin <mansidikonda@gmail.com>
2024-10-01 13:33:21 +0530

    update

--- llm_chains.py
+++ llm_chains.py
@@ -1,3 +1,4 @@
+from prompt_templates import memory_prompt_template
 from langchain.chains import StuffDocumentsChain, LLMChain, ConversationalRetrievalChain
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.memory import ConversationBufferWindowMemory
@@ -11,8 +12,8 @@ import yaml
 with open("config.yml","r") as f:
     config = yaml.safe_load(f)
     
-def create_llm(model_path = config["model_path"]["large"], model_type = config["model_type"]):
-    llm = CTransformers(model_path, model_type)
+def create_llm(model_path = config["model_path"]["large"], model_type = config["model_type"], model_config = config["model_config"]):
+    llm = CTransformers(model = model_path, model_type = model_type, config = model_config)
     return llm
 
 def create_embeddings(embeddings_path = config["embeddings_path"]):
@@ -24,9 +25,19 @@ def create_chat_memory(chat_history):
 def create_prompt_from_template(template):
     return PromptTemplate.from_template(template)
 
-def create_llm_chain():
-    for i in range(9):
-        print("k")
+def create_llm_chain(llm, chat_prompt, memory):
+    return LLMChain(llm = llm, prompt = chat_prompt, memory = memory)
     
-def load_normal_chain():
-    print("heloooooo")
+def load_normal_chain(chat_history):
+    return chatChain(chat_history)
+    
+class chatChain:
+    def __init__(self, chat_history):
+        self.memory = create_chat_memory(chat_history)
+        llm = create_llm()
+        chat_prompt = create_prompt_from_template(memory_prompt_template)
+        self.llm_chain = create_llm_chain(llm, chat_prompt, self.memory)
+    
+    def run(self, user_input):
+        return self.llm_chain.run(human_input = user_input, history = self.memory.chat_memory.messages, stop=["Human:"])
+    
